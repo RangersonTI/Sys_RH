73ce1dfa6a120451d8982e2bb8e38a55c0696506
luis felipe cardoso marinho <126625142+luisfelipecad@users.noreply.github.com>
2024-07-04 16:16:58 -0300

    models

--- rh/admin.py
+++ rh/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Funcionario, Cargos
 
-# Register your models here.
+admin.site.register(Funcionario)
+admin.site.register(Cargos)
--- rh/models.py
+++ rh/models.py
@@ -10,3 +10,14 @@ class Funcionario(models.Model):
     def __str__(self):
         return f'idade: {self.idade} | data: {self.data}'
 
+class Cargos(models.Model):
+    cargo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+    departamento = models.CharField(max_length=100)
+    salarioBase = models.CharField(max_length=100)
+    requisitoDeformacao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'cargo: {self.cargo} | departamento: {self.departamento}'
+
+ 
