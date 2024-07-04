522f3c5c390de36670fb17ed93213c619cc0080a
RangersonTI <rangerson14@outlook.com>
2024-07-04 17:02:07 -0300

    alteração do tipo do atributo salario_base - models

--- rh/models.py
+++ rh/models.py
@@ -14,10 +14,8 @@ class Cargos(models.Model):
     cargo = models.CharField(max_length=100)
     descricao = models.CharField(max_length=100)
     departamento = models.CharField(max_length=100)
-    salarioBase = models.CharField(max_length=100)
+    salarioBase = models.DecimalField(max_digits=10, decimal_places=2)
     requisitoDeformacao = models.CharField(max_length=100)
 
     def __str__(self):
         return f'cargo: {self.cargo} | departamento: {self.departamento}'
-
- 
